f34cce6c0a5e31112e9ec73188cfb2743672d0f1
oatmeala <oatmeala31323123@gmail.com>
2023-08-04 17:14:48 +0200

    inital code

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+
+use serenity::{
+    prelude::*,
+};
+
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN")
+        .expect("Expected a token in the environment");
+    let intents = GatewayIntents::all();
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
